bc1c5f7d6811a9d9351a52a48ae104c2da42a435
cgokce01 <canangokce01@gmail.com>
2023-09-20 19:54:20 +0000

    update

---
+++ installation.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+ git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv                
+ echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
+ echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
+ sudo ln -s ~/.tfenv/bin/* /usr/local/bin
+ 
