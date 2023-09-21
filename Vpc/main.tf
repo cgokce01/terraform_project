94fd21b1818299b23507c1fc21b8f406779aaf23
cgokce01 <canangokce01@gmail.com>
2023-09-21 05:32:43 +0000

    updated the files

--- Vpc/main.tf
+++ Vpc/main.tf
@@ -7,28 +7,17 @@ tags = {
 }
 
  # CREATING PUBLIC SUBNETS
-resource "aws_subnet" "public_subnetS"{
+resource "aws_subnet" "vpc_subnets"{
     vpc_id                  = aws_vpc.project-terraform.id
     cidr_block              = var.cidr_block[count.index]
     availability_zone       = var.az[count.index]
     map_public_ip_on_launch = true
-    count                   = 3
+    count                   = 6
 
     tags = {
-      name = "public_subnets"
+      name = "vpc_subnets"
     }
 }
 
 
-  # CREATING PRIVATE SUBNETS
-resource "aws_subnet" "private_subnets"{
-    vpc_id              = aws_vpc.project-terraform.id
-    cidr_block          = var.cidr_block[count.index]
-    availability_zone   = var.az[count.index]
-    count               = 3
-    
-    tags = {
-      name = "private_subnets"
-    }
 
-}
--- Vpc/variables.tf
+++ Vpc/variables.tf
@@ -7,15 +7,15 @@ variable "region" {
 variable "cidr_block" {
   description = "please provide a cidr_block information"
   type        = list(string)
-  default     = ["10.0.7.0/24", "10.0.8.0/24","10.0.9.0/24"
-                 "10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"  ]
+  default     = ["10.0.4.0/24", "10.0.5.0/24","10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
 }
+
 
 
 variable "az" {
   type        = list(string)
   description = "Availability Zones"
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
 }
 
 
