dc84bcf30ca79ff230cc9b0562f6f41f6ef6ef09
cgokce01 <canangokce01@gmail.com>
2023-09-21 05:19:56 +0000

    updated variables

--- Vpc/variables.tf
+++ Vpc/variables.tf
@@ -7,7 +7,8 @@ variable "region" {
 variable "cidr_block" {
   description = "please provide a cidr_block information"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24","10.0.3.0/24"]
+  default     = ["10.0.7.0/24", "10.0.8.0/24","10.0.9.0/24"
+                 "10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"  ]
 }
 
 
@@ -17,16 +18,4 @@ variable "az" {
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
-
-variable "vpc_public_subnets" {
-  description = "List of public subnet CIDR ranges"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "vpc_private_subnets" {
-  description = "List of public subnet CIDR ranges"
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
 
