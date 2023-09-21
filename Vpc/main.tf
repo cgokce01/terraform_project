08c932425bfe96128e66fa43fa6a3031569b50e2
cgokce01 <canangokce01@gmail.com>
2023-09-21 05:50:32 +0000

    updated

--- Vpc/main.tf
+++ Vpc/main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_subnets"{
     cidr_block              = var.cidr_block[count.index]
     availability_zone       = var.az[count.index]
     map_public_ip_on_launch = true
-    count                   = 3
+    count                   = 3 (var.cidr_block)
 
     tags = {
       name = "public_subnets"
@@ -24,7 +24,7 @@ resource "aws_subnet" "private_subnets"{
     vpc_id              = aws_vpc.project-terraform.id
     cidr_block          = var.cidr[count.index]
     availability_zone   = var.az[count.index]
-    count               = 3
+    count               = 3(var.cidr)
     tags = {
       name = "private_subnets"
     }
