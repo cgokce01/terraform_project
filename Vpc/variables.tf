32d1aa8349afcbf44acae6a55e279908c9193ce5
cgokce01 <canangokce01@gmail.com>
2023-09-21 05:59:59 +0000

    updated

--- Vpc/variables.tf
+++ Vpc/variables.tf
@@ -12,7 +12,7 @@ variable "vpc_cidr_block" {
 
 
 variable "cidr_block" {
-  description = "please provide a cidr_block information"
+  description = "please provide a cidr_block for public subnets"
   type        = list(string)
   default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24", ]
 }
