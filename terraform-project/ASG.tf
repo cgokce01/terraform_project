0cd47b81293e90d28d646877eb69065e17b80742
cgokce01 <canangokce01@gmail.com>
2023-09-21 07:20:53 +0000

    updated alb

--- terraform-project/ASG.tf
+++ terraform-project/ASG.tf
@@ -9,12 +9,6 @@ resource "aws_lb" "load-balancer" {
 
   enable_deletion_protection = true
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
-
   tags = {
     Environment = "production"
   }
