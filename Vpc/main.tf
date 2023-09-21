7d2d7343f0441e2161ca4e66ae7fe0092bb721fb
cgokce01 <canangokce01@gmail.com>
2023-09-21 06:55:46 +0000

    added sg

--- Vpc/main.tf
+++ Vpc/main.tf
@@ -90,4 +90,40 @@ resource "aws_route_table" "project-rt2" {
   }
 }
 
+ # Create a route table association for private subnets
+resource "aws_route_table_association" "route-private_subnets" {
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.project-rt2.id
+  count = 3
+}
 
+ # Create a security group
+resource "aws_security_group" "project-sg" {
+  name        = "project-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      =  aws_vpc.project-terraform.id
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+    ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "project-sg"
+  }
+}
