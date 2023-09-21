3d4eaaee13474c773542707b19bab2fb9d001830
cgokce01 <canangokce01@gmail.com>
2023-09-21 06:18:42 +0000

    updated

--- Vpc/main.tf
+++ Vpc/main.tf
@@ -33,9 +33,29 @@ resource "aws_subnet" "private_subnets"{
 # Create a internet-gateway
 resource "aws_internet_gateway" "project-igw" {
   vpc_id = aws_vpc.project-terraform.id
-  
+
   tags = {
     name = "project-igw"
   }
 }
+
+ # Create a route table internet gateway
+resource "aws_route_table" "project-rt" {
+  vpc_id = aws_vpc.project-terraform.id
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.project-igw.id
+  }
+  tags = {
+    Name = "project-rt"
+  }
+}
+
+ # Create a route table association
+resource "aws_route_table_association" "route-public_subnets" {
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.project-rt.id
+  count = 3
+}
+
 
