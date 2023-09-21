150a6ca8e9de212a28c27ffd48a05d4ae27b4e42
cgokce01 <canangokce01@gmail.com>
2023-09-21 07:38:43 +0000

    added target group

--- terraform-project/ASG.tf
+++ terraform-project/ASG.tf
@@ -3,13 +3,33 @@
 resource "aws_lb" "load-balancer" {
   name               = "project-alb"
   internal           = false
-  load_balancer_type = "ipv4"
+  load_balancer_type = "application"
   security_groups    = [aws_security_group.project-sg.id]
   subnets            = [aws_subnet.public_subnets[0].id, aws_subnet.public_subnets[1].id, aws_subnet.public_subnets[2].id]
 
   enable_deletion_protection = true
 
   tags = {
-    Environment = "production"
+    Environment = "project-alb"
   }
 }
+
+## Target Group
+resource "aws_lb_target_group" "target-group" {
+  name     = "project-tg"
+  port     = 80
+  protocol = "HTTP"
+  target_type = "instance"
+  vpc_id   = aws_vpc.project-terraform.id 
+}
+
+  health_check {
+    path                = "/"
+    port                = 80
+    protocol            = "HTTP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    check_interval_sec  = 10
+    timeout_sec         = 5
+    matcher             = "200"
+  }
