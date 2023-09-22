99aa2e73f70be67e5d053e099c16f35ffac136d8
cgokce01 <canangokce01@gmail.com>
2023-09-22 18:29:04 +0000

    added

--- terraform-project/ASG.tf
+++ terraform-project/ASG.tf
@@ -56,10 +56,9 @@ resource "aws_key_pair" "project_keypair" {
 resource "aws_instance" "instances" {
   ami           = "ami-00c6177f250e07ec1"
   instance_type = "t2.micro"
-  key_name = "project"
+  key_name = "project_keypair"
   subnet_id = aws_subnet.public_subnets[count.index].id
-  vpc_security_group_ids = [aws_security_group.project-sg]
-  type        = list(string)
+  vpc_security_group_ids = [aws_security_group.project-sg].id
   associate_public_ip_address = true
   count = 3
 
